b7b696890ca4a344937e1ca7e6df4636123873e1
beucherie remi <beucherie.r@gmail.com>
2021-01-20 14:01:48 +0100

    react app like airbnb

---
+++ src/components/flat.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Flat.css';
+
+class Flat extends React.Component {
+  handleClick = () => {
+    this.props.selectFlat(this.props.flat);
+  };
+
+  render(){
+    const title = `${this.props.flat.price} ${this.props.flat.priceCurrency}  ${this.props.flat.name}`;
+
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    };
+
+    return(
+      <div className="flat" onClick={this.handleClick}>
+        <div className="flat-picture" style={style}></div>
+        <div className="flat-title">{title}</div>
+      </div>
+      );
+  }
+}
+
+export default Flat;
